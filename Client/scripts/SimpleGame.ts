267e11eab77c901e58b162ba577c270abc267ae6
Fernando Perez <gunderwulde@hotmail.es>
2016-03-21 12:57:01 +0100

    - Proyecto inicial.

---
+++ Client/scripts/SimpleGame.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path="typings/phaser/phaser.d.ts"/>
+
+class SimpleGame {
+
+    constructor() {
+//        this.game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.image('logo', 'images/phaser2.png');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+
+}

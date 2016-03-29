89c512990ad7c33476320880dd2bdfe95b0244df
Fernando Perez <gunderwulde@hotmail.es>
2016-03-29 12:50:35 +0200

    - Eliminado antiguo poryecto WebClient

--- ClientWeb/scripts/web/index.ts
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="../typings/phaser/phaser.d.ts"/>
-/// <reference path='../typings/firebase/firebase.d.ts'/>
-
-module YourTurn {
-    "use strict";
-    window.onload = function () {
-        var game = new EntryPoint();
-    }
-}

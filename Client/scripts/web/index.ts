c004bc55dc21b64b81066780032054e8b48b3d8c
Fernando Perez <gunderwulde@hotmail.es>
2016-03-29 12:51:31 +0200

    - Nuevo proyecto Cliente Web en el mismo direcrtorio que el cliente Cordova.

---
+++ Client/scripts/web/index.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../typings/phaser/phaser.d.ts"/>
+/// <reference path='../typings/firebase/firebase.d.ts'/>
+
+module YourTurn {
+    "use strict";
+    window.onload = function () {
+        var game = new EntryPoint();
+    }
+}

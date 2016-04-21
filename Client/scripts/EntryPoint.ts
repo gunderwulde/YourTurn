267ca2497d4c8fd08ad2dc96fef77cfe40c90360
Fernando Perez <gunderwulde@hotmail.es>
2016-04-21 10:54:52 +0200

    - Opción para poder mover la app a la sd.

--- Client/scripts/EntryPoint.ts
+++ Client/scripts/EntryPoint.ts
@@ -7,9 +7,13 @@
                 create: ()=>{
                     this.cordova = typeof (<any>window).cordova !== 'undefined';
                     //  This sets a limit on the up-scale
+
+
                     this.scale.maxWidth = window.innerWidth * window.devicePixelRatio;
                     this.scale.maxHeight = window.innerHeight * window.devicePixelRatio;
                     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+                    this.scale.windowConstraints.bottom = "visual";
+                    this.scale.pageAlignHorizontally = true;
         //            this.scale.aspectRatio = 720 / 1280;
                     this.input.maxPointers = 1;
                     this.stage.disableVisibilityChange = true;
